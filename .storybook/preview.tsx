682b10a1ae153cdf9d6666c25a0a2f2d923a140d
Josh Allan <joshuaallan@live.co.uk>
2026-03-09 21:35:15 +0000

    chore: initial commit of component library

---
+++ .storybook/preview.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import type { Preview } from "@storybook/react";
+import { themes } from "storybook/theming";
+import "../src/lib/styles/main.css";
+
+import "@fontsource/geist-sans/400.css";
+import "@fontsource/geist-sans/700.css";
+import "@fontsource/geist-sans/800.css";
+import "@fontsource/geist-mono/400.css";
+import "@fontsource/geist-mono/900.css";
+
+import "../src/lib/styles/layout.css";
+import "../src/lib/styles/reset.css";
+import "../src/lib/components/Typography/Typography.module.css";
+
+const preview: Preview = {
+  parameters: {
+    layout: "fullscreen",
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: { disable: true },
+    options: {
+      storySort: {
+        order: ["Foundations", "Layout", "Components", ["Cards", "*"], "*"],
+      },
+    },
+  },
+  decorators: [(Story) => <Story />],
+};
+
+export default preview;
